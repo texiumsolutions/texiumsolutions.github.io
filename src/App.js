3930096c9dcb76481af706f6b283b4db0faaf5b6
texiumsolutions <93979453+Arif11819@users.noreply.github.com>
2023-01-18 20:28:48 +0600

    added file after complete some route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
+import Navbar from './Home/Navbar/Navbar';
+import Login from './Home/Navbar/Login';
+import Signup from './Home/Navbar/Signup';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
       </Routes>
     </div>
   );
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1 className='text-2xl'>Welcome to our Texium Solutions Dashboard</h1>
         </div>
     );
 };
