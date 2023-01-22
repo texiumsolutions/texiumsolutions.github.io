24db5a7ee957b950a2f789bfa931fbe5cc0d238c
texiumsolutions <93979453+Arif11819@users.noreply.github.com>
2023-01-22 19:27:06 +0600

    added file now

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,7 +6,7 @@ import "../App.css";
 const Home = () => {
     return (
         <div className='flex h-96 justify-center items-center'>
-            <h1 className='text-4xl text-info font-bold'>Welcome to Texium Data Platform! <br /> <Link to='/login' className='text-2xl text-blue-800 flex justify-center items-center lg:py-6'>Login Here <span className='mx-3 my-1'><BsFillArrowRightSquareFill></BsFillArrowRightSquareFill></span></Link></h1>
+            <h1 className='text-4xl text-black font-bold'>Welcome to Texium Data Platform! <br /> <Link to='/login' className='text-2xl text-blue-800 flex justify-center items-center lg:py-6'>Login Here <span className='mx-3 my-1'><BsFillArrowRightSquareFill></BsFillArrowRightSquareFill></span></Link></h1>
 
         </div>
     );
